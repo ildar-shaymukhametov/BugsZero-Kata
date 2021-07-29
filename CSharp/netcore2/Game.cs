f57ca32c0a5d0118b451524f93338bd68962bdf9
Шаймухаметов Ильдар <ildar.shaymukhametov@gmail.com>
2021-07-29 09:26:34 +0500

    remove parentheses

--- CSharp/netcore2/Game.cs
+++ CSharp/netcore2/Game.cs
@@ -28,8 +28,8 @@ namespace Trivia
             for (int i = 0; i < 50; i++)
             {
                 popQuestions.AddLast("Pop Question " + i);
-                scienceQuestions.AddLast(("Science Question " + i));
-                sportsQuestions.AddLast(("Sports Question " + i));
+                scienceQuestions.AddLast("Science Question " + i);
+                sportsQuestions.AddLast("Sports Question " + i);
                 rockQuestions.AddLast("Rock Question " + i);
             }
         }
